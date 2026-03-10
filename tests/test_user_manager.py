465151d30c4c0eba4ab64a9f69de157577ac869d
hanae-kiki <kikihanae5@gmail.com>
2026-03-10 16:27:17 +0100

    k

--- tests/test_user_manager.py
+++ tests/test_user_manager.py
@@ -1,25 +1,32 @@
 import pytest
 from app.user_manager import UserManager
+
+
 def test_add_user():
-  manager = UserManager()
-  manager.add_user("hanae-kiki")
-  assert manager.count_user()==1
-  
-  
-def test_add_existing_user():
-  manager = UserManager()
-  manager.add_user("hanae-kiki")  
-  with pytest.raises(ValueError):
+    manager = UserManager()
     manager.add_user("hanae-kiki")
-    
+
+    assert manager.count_users() == 1
+
+
+def test_add_existing_user():
+    manager = UserManager()
+    manager.add_user("hanae-kiki")
+
+    with pytest.raises(ValueError):
+        manager.add_user("hanae-kiki")
+
+
 def test_remove_user():
-  manager = UserManager()
-  manager.add_user("oumaima")
-  manager.remove_user("oumaima")
-  assert manager.count_user()==0
-  
-def test_remove_unknow_user():
-  manager = UserManager()
-  with pytest.raises(ValueError):
-    manager.remove_user("saad")
-  
+    manager = UserManager()
+    manager.add_user("oumaima")
+    manager.remove_user("oumaima")
+
+    assert manager.count_users() == 0
+
+
+def test_remove_unknown_user():
+    manager = UserManager()
+
+    with pytest.raises(ValueError):
+        manager.remove_user("saad")
